c42f7860a02966f531c2ac53985cac0f709c830e
QQtaQQta <yuyutarasenko@gmail.com>
2023-09-25 22:09:21 +0700

    math3

--- math.py
+++ math.py
@@ -6,7 +6,7 @@ x = float(input('enter x: '))
 
 z1 = (math.cos(math.radians(x)) + math.sin(math.radians(x))) / (math.cos(math.radians(x)) - math.sin(math.radians(x)))
 
-z2 = math.tan(math.radians(x*2)) - 1 / math.cos(math.radians(x*2))
+z2 = math.tan(math.radians(x*2)) + 1 / math.cos(math.radians(x*2))
 
 print("z1 = ", z1)
 print("z2 = ", z2)
